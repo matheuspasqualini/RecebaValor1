fea6ba118ea0ec01d5a43d14b1604ca23f4ce3f5
740892 - MATHEUS PASQUALINI <740892@unilab.fsa.br>
2022-04-05 22:31:47 -0300

    first commit

---
+++ RecebaValor1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    int tabuada = 0;
+    
+    printf("\nDigite a tabuada que você deseja  ");
+    scanf("%i", &tabuada);
+
+    for(int x = 1; x<=10; ++x){
+        //1 x 5 = 5
+        printf("%ix%i = %i\n", x, tabuada, x * tabuada);
+    }
+
+    return 0;
+}
